2303ab8376ae6ab9afc9db84e92ca94cb551f7c2
Javier Sanchez Ventero <js@ventero.es>
2014-07-06 20:49:17 +0200

    Update Soap.php
    Changes to adapt the Soap Driver to work with the ISPConfig authenticated SOAP server

--- passwd/lib/Driver/Soap.php
+++ passwd/lib/Driver/Soap.php
@@ -47,26 +47,47 @@ class Passwd_Driver_Soap extends Passwd_Driver
 
     /**
      */
-    protected function changePassword($user, $oldpass, $newpass)
+    protected function _changePassword($user, $oldpass, $newpass)
+    //* la funcion estaba mal definida. No hubiera funcionado sin el ._.
     {
-        $args = array();
+        /*
+         * Se modifica la variable args para adaptarla a la llamada de los metodos
+         * de ISPConfig
+         */
+        $args = array('session_id'=>'','1','params' => array());
+
+        //* Se colocan los parametros de la funcion en el sitio definido por
+        //* array "arguments"
+        //* Esto es casi igual que como estaba, solo que ahora los argumentos
+        //* van dentro de un array
+
         if (($pos = array_search('username', $this->_params['arguments'])) !== false) {
-            $args[$pos] = $user;
+            $args['params'][$pos] = $user;
         }
         if (($pos = array_search('oldpassword', $this->_params['arguments'])) !== false) {
-            $args[$pos] = $oldpass;
+            $args['params'][$pos] = $oldpass;
         }
         if (($pos = array_search('newpassword', $this->_params['arguments'])) !== false) {
-            $args[$pos] = $newpass;
+            $args['params'][$pos] = $newpass;
         }
 
+        //* Abrimos el cliente como indica el metodo general
         $client = new SoapClient(
             $this->_params['wsdl'],
             $this->_params['soap_params']
         );
-        $res = $client->__soapCall($this->_params['method'], $args);
-        if ($res instanceof SoapFault) {
-            throw new Passwd_Exception($res->getMessage(), $res->getCode());
+
+        //* Abrimos una sesion con el usuario y password definidos en el array login
+        if  ($session_id = $client->login(
+                 $this->_params['login']['username'],
+                 $this->_params['login']['password']
+             )){
+            //+ Si todo va bien, hacemos la llamada al metodo como estaba antes.
+            $args['session_id'] = $session_id;
+            $res = $client->__soapCall($this->_params['method'], $args);
+            if ($res instanceof SoapFault) {
+                throw new Passwd_Exception($res->getMessage(), $res->getCode());
+            }
         }
     }
 
